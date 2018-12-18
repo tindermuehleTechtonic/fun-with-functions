2a4544afeccb120242df418700c3ef8830446055
Brett <bgoers@techtonicgroup.com>
2018-12-18 10:25:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+$(".answerKey").attr("style","display: none;") //DO NOT REMOVE this is just to hide the empty solution and feedback div that we will add after grading
+//Please enter your name for the assignment below
+document.getElementById('user').innerHTML =
+'Your name goes here'
+
+//problem-1
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-1").innerHTML =
+
+
+//problem-2
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-2").innerHTML =
+
+//problem-3
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-3").innerHTML =
+
+
+//problem-4
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-4").innerHTML =
+
+
+//problem-5
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-5").innerHTML =
+
+
+//problem-6
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-6").innerHTML =
+
+
+//problem-7
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-7").innerHTML =
+
+
+//problem-8
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-8").innerHTML =
+
+
+//problem-9
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("problem-9").innerHTML =
+
+
+//challenge-1
+
+// place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
+// document.getElementById("challenge-1.1").innerHTML = addTheWorld(5,2,8)
+// document.getElementById("challenge-1.2").innerHTML = addTheWorld(10,5,3,2)
+// document.getElementById("challenge-1.3").innerHTML = addTheWorld(25)
