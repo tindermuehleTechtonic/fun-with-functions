e707a212f38957ccf71a529e621a7a5c37455955
Tim Indermuehle <tim.indermuehle@techtonic.com>
2019-01-22 14:24:50 -0700

    completed problem 9

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ document.getElementById("problem-3").innerHTML = showResultsInBody(5);
 
 //problem-4
 function showResults(prameterName) {
-  return "The result of your calculation is " + prameterName;
+  return "The result of your calculation is " + prameterName + ".";
 }
 // place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
 document.getElementById("problem-4").innerHTML = showResults(5);
@@ -46,27 +46,35 @@ document.getElementById("problem-5").innerHTML = multiplyNumbers(5, 10)
 
 
 //problem-6
-
+function divideNumbers(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
 // place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
-// document.getElementById("problem-6").innerHTML =
+document.getElementById("problem-6").innerHTML = divideNumbers(20, 4);
 
 
 //problem-7
+function showResults(prameterName) {
+
+  return "The result of your calculation is " + prameterName + ".";
+}
 
 // place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
-// document.getElementById("problem-7").innerHTML =
+document.getElementById("problem-7").innerHTML = showResults(addNumbers(3, 5));
 
 
 //problem-8
 
+
+
 // place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
-// document.getElementById("problem-8").innerHTML =
+document.getElementById("problem-8").innerHTML = addNumbers(5, 6) + addNumbers(7, 3);
 
 
 //problem-9
 
 // place functions and supporting code here.  Uncomment line below and place answer the to right of = sign
-// document.getElementById("problem-9").innerHTML =
+document.getElementById("problem-9").innerHTML = multiplyNumbers(15, 4) - divideNumbers(10, 2);
 
 
 //challenge-1
